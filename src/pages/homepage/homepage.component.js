1eb2c32d881329178298547d12c1d6f0d2a95a32
kildre <kildre@gmail.com>
2021-01-14 10:00:11 -0600

    updated homepag title

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -5,7 +5,7 @@ import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <div className="homepage">
-    <h1>Welcome to my Homepage</h1>
+    <h1 className="title">{"Killer Clothing".toUpperCase()}</h1>
     <Directory />
   </div>
 );
