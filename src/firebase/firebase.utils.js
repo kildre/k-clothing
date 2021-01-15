87e2229fc91204aa6630371eed6e7f101a3927be
kildre <kildre@gmail.com>
2021-01-15 16:09:54 -0600

    added google authentication and firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCUpqc6jas1s7N8_5sAxxcffgTaXWHAIOU",
+  authDomain: "k-clothing-aa572.firebaseapp.com",
+  projectId: "k-clothing-aa572",
+  storageBucket: "k-clothing-aa572.appspot.com",
+  messagingSenderId: "642882059185",
+  appId: "1:642882059185:web:4f39b3d49df957499e9b90",
+  measurementId: "G-C3QKCP0ZG6",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
