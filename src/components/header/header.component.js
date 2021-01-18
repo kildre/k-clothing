5e431f9fab796fc2975fb3db381a1d23605ea672
kildre <kildre@gmail.com>
2021-01-18 12:10:09 -0600

    added google sign in OAuth

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crownKB.svg";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" width={"120px"} height={"120px"} />
@@ -19,6 +21,15 @@ const Header = () => (
       <Link className="option" to="/cart">
         {"cart".toUpperCase()}
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          {"Sign Out".toUpperCase()}
+        </div>
+      ) : (
+        <Link className="option" to="/sign-in">
+          SIgn In
+        </Link>
+      )}
     </div>
     <div className="attribute">
       Icons made by{" "}
