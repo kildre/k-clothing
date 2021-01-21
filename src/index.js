2c8ef49a41b716b76cc604580d05a7678347c81b
kildre <kildre@gmail.com>
2021-01-21 13:13:34 -0600

    added redux first stages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
+import { Provider, useStore } from "react-redux";
 
+import store from "./redux/store";
+
+import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
